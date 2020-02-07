50e3f325ffb1b8b3fbbf7133ea829654abb80276
nksoluna <xitnikz@gmail.com>
2020-02-07 16:05:47 +0700

    Pokemon version2

--- PokemonGame.java
+++ PokemonGame.java
@@ -1,4 +1,10 @@
 import java.util.Scanner;
 import java.io.*;
 
-public class PokemonGame
+public class PokemonGame {
+
+    public static void main (String arg[] {
+        System.out.println("Welcome to The Pokemon Game !!") ;
+        System.out.println("Game Started !!") ;
+        Pokemon
+}
