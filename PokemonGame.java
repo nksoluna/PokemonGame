2b432488c064eb62406bc0f4f60a1f8cc142d6e1
nksoluna <xitnikz@gmail.com>
2020-02-07 16:26:55 +0700

    Pokemon version3

--- PokemonGame.java
+++ PokemonGame.java
@@ -6,5 +6,20 @@ public class PokemonGame {
     public static void main (String arg[] {
         System.out.println("Welcome to The Pokemon Game !!") ;
         System.out.println("Game Started !!") ;
-        Pokemon
+        Pokemon Zacian = new Pokemon("Zacian") ;
+        while (true) {
+            system.out.println(Zacian.getStats());
+            Zacian.exp(300);
+            wait (20000) ;
+        }
+}
+
+private static void wait(int miliseconds)
+    {
+        try {
+            Thread.sleep(miliseconds);
+        }
+        catch (Exception e) { }
+    }
+
 }
