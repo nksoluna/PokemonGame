42317029ee10de98ca56099f3d1be7be1a367be5
nksoluna <xitnikz@gmail.com>
2020-04-27 15:01:47 +0700

    Version3

--- PokemonGame.java
+++ PokemonGame.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 public class PokemonGame   {
 
@@ -13,21 +13,3 @@ public class PokemonGame   {
     }
 
 }  
-=======
-import java.util.*;
-import java.util.ArrayList;
-
-
-public class PokemonGame {
-
-    public static void main (String arg[]) {
-        System.out.println("Welcome to The Pokemon Game !!") ;
-
-        Trainer t = new Trainer("Trainer Name : Red");
-        //MainGame mg = new MainGame(t) ;
-         t.play() ;
-        System.out.println("End Pokemon Game") ; 
-        
-    }
-}
->>>>>>> 4359bc21fc8eeb1a93576d7e167b50a1fca91158
--- Readme.md
+++
@@ -1,1 +0,0 @@
---------------------------This is The file for test about Pokemon---------------
